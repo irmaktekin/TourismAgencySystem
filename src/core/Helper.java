e5588255b9c018f6427a3487bf29661dad9d0684
irmaktekin <irmaktekin97@gmail.com>
2024-06-18 01:48:15 +0300

    Method is created for showing application messages.

--- src/core/Helper.java
+++ src/core/Helper.java
@@ -1,5 +1,6 @@
 package core;
 
+import javax.swing.*;
 import java.awt.*;
 
 public class Helper {
@@ -10,5 +11,36 @@ public class Helper {
             case "y" -> (Toolkit.getDefaultToolkit().getScreenSize().height - size.height) / 2;
             default -> 0;
         };
+    }
+
+    public static void displayMessage(String str) {
+        String msg;
+        String title = switch (str) {
+            case "done" -> {
+                msg = "Success";
+                yield "Result";
+            }
+            case "fill" ->{
+                msg = "Please fill mandatory fields!";
+                yield "Alert!";
+            }
+            case "notfound" ->{
+                msg = "User not found!";
+                yield "Login Failed!";
+            }
+            default -> {
+                msg = str;
+                yield "Message";
+            }
+        };
+        JOptionPane.showMessageDialog(null,msg,title,JOptionPane.INFORMATION_MESSAGE);
+    }
+    public static boolean isFieldListBlank(JTextField[] fieldList){
+        for(JTextField field : fieldList){
+            if (field.getText().trim().isEmpty()){
+                return true;
+            }
+        }
+        return false;
     }
 }

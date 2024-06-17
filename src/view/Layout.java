9ea819c0a178f0ebe6431ca6a3bda108132bb90e
irmaktekin <irmaktekin97@gmail.com>
2024-06-18 01:14:14 +0300

    Method is created for initializing GGUI.

--- src/view/Layout.java
+++ src/view/Layout.java
@@ -1,2 +1,15 @@
-package view;public class Layout {
+package view;
+
+import core.Helper;
+
+import javax.swing.*;
+
+public class Layout extends JFrame {
+    public void initalizeGui(int width,int height){
+        this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        this.setTitle("Tourism Agency System");
+        this.setSize(width,height);
+        this.setLocation(Helper.getLocationPoint("x",this.getSize()),Helper.getLocationPoint("y",this.getSize()));
+        this.setVisible(true);
+    }
 }

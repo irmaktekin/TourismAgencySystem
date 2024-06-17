fd4beb78135538209d2a7cd60b10b4cf3e453e6c
irmaktekin <irmaktekin97@gmail.com>
2024-06-18 00:47:17 +0300

    Login view layout is designed.

---
+++ src/view/LoginView.java
@@ -0,0 +1,16 @@
+package view;
+
+import javax.swing.*;
+
+public class LoginView {
+    private JPanel container;
+    private JLabel lbl_welcome;
+    private JLabel lbl_login;
+    private JPanel p_header;
+    private JPanel p_body;
+    private JLabel lbl_username;
+    private JTextField textField1;
+    private JLabel lbl_password;
+    private JTextField textField2;
+    private JButton loginButton;
+}

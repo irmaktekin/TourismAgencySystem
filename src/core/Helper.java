dd12699653855a1e64867999df3b8ed7d1bd647e
irmaktekin <irmaktekin97@gmail.com>
2024-06-18 01:15:32 +0300

    Method for getting the center point is created.

--- src/core/Helper.java
+++ src/core/Helper.java
@@ -1,2 +1,14 @@
-package core;public class Helper {
+package core;
+
+import java.awt.*;
+
+public class Helper {
+    //load component to the center of the screen
+    public static int getLocationPoint(String type, Dimension size){
+        return switch (type) {
+            case "x" -> (Toolkit.getDefaultToolkit().getScreenSize().width - size.width) / 2;
+            case "y" -> (Toolkit.getDefaultToolkit().getScreenSize().height - size.height) / 2;
+            default -> 0;
+        };
+    }
 }

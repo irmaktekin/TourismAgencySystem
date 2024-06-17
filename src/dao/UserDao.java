d6af5699e9ba36b892b4805c474f38e50ae5bbec
irmaktekin <irmaktekin97@gmail.com>
2024-06-18 02:06:00 +0300

    Login method is created.

--- src/dao/UserDao.java
+++ src/dao/UserDao.java
@@ -4,6 +4,7 @@ import core.DbConnector;
 import entity.User;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -37,10 +38,28 @@ public class UserDao {
             user.setUser_name(rs.getString("user_name"));
             user.setEmail(rs.getString("email"));
             user.setPassword(rs.getString("user_password"));
-            user.setRole(rs.getString("role"));
+            user.setRole(rs.getString("user_role"));
         }
         catch (SQLException e){
             throw new RuntimeException(e);
+        }
+        return user;
+    }
+    public User login(String username, String password){
+        User user = null;
+        String query = "Select * From public.user_ Where user_name = ? And user_password = ?";
+        try{
+            PreparedStatement pr = this.con.prepareStatement(query);
+            pr.setString(1,username);
+            pr.setString(2,password);
+            ResultSet resultSet = pr.executeQuery();
+            if(resultSet.next()){
+                user = this.mapResultSetToUser(resultSet);
+            }
+        }
+        catch (SQLException e)
+        {
+        e.printStackTrace();
         }
         return user;
     }

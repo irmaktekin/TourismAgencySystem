d8fdf7a7eea3461e381988c7342f3f43b6417677
irmaktekin <irmaktekin97@gmail.com>
2024-06-18 17:33:45 +0300

    Created for getting the values of combo box items.

--- src/core/ComboItem.java
+++ src/core/ComboItem.java
@@ -1,2 +1,15 @@
-package core;public class ComboItem {
+package core;
+
+public class ComboItem {
+    private int key;
+    private String value;
+    public ComboItem(int key,String value){
+        this.value=value;
+        this.key = key;
+    }
+
+    @Override
+    public String toString() {
+        return this.value;
+    }
 }

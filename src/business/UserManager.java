d067de9c8edc9b3a58fec9c19affb9f533715786
irmaktekin <irmaktekin97@gmail.com>
2024-06-18 03:15:48 +0300

    Method is created for getting the rows.

--- src/business/UserManager.java
+++ src/business/UserManager.java
@@ -3,6 +3,8 @@ package business;
 import dao.UserDao;
 import entity.User;
 
+import java.util.ArrayList;
+
 public class UserManager {
     private final UserDao userDao;
 
@@ -11,5 +13,24 @@ public class UserManager {
     }
     public User login(String username, String password){
         return this.userDao.login(username, password);
+    }
+    public ArrayList<User> getAllUsers(){
+        return this.userDao.getAllUsers();
+    }
+    public ArrayList<Object[]> getForTable(int size,ArrayList<User> users){
+        ArrayList<Object[]> userList = new ArrayList<>();
+        for(User obj : users){
+            int i = 0;
+            Object[] rowObject = new Object[size];
+            rowObject[i++] = obj.getUser_id();
+            rowObject[i++] = obj.getUser_name();
+            rowObject[i++] = obj.getEmail();
+            rowObject[i++] = obj.getPassword();
+            rowObject[i++] = obj.getRole();
+
+            userList.add(rowObject);
+
+        }
+        return userList;
     }
 }

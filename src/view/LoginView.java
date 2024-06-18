fdf9e23b481c739f8fe1bff5823a1d81c457e9c4
irmaktekin <irmaktekin97@gmail.com>
2024-06-18 17:34:58 +0300

    Redirecting operations based on the user role is performed.

--- src/view/LoginView.java
+++ src/view/LoginView.java
@@ -1,16 +1,49 @@
 package view;
 
+import business.UserManager;
+import core.Helper;
+import entity.User;
+
 import javax.swing.*;
 
-public class LoginView {
+public class LoginView extends Layout {
     private JPanel container;
     private JLabel lbl_welcome;
     private JLabel lbl_login;
     private JPanel p_header;
     private JPanel p_body;
     private JLabel lbl_username;
-    private JTextField textField1;
+    private JTextField fld_username;
     private JLabel lbl_password;
-    private JTextField textField2;
+    private JTextField fld_password;
     private JButton loginButton;
+    private UserManager userManager;
+
+    public LoginView(){
+        this.userManager = new UserManager();
+        this.add(container);
+        this.initalizeGui(400,400);
+        loginButton.addActionListener(e-> {
+           JTextField [] fieldList = {this.fld_username,this.fld_password};
+           if(Helper.isFieldListBlank(fieldList)){
+               Helper.displayMessage("fill");
+           }
+           else{
+               User user = this.userManager.login(this.fld_username.getText(),this.fld_password.getText());
+               if(user == null){
+                   Helper.displayMessage("notfound");
+               }
+               else if(user.getRole().equals("accounting")){
+                   System.out.println("Accounting user");
+               }
+               else{
+                   dispose();
+                   AdminView adminView = new AdminView(user);
+               }
+           }
+
+        });
+    }
+
+
 }

575a168ced47778441ea01f0a0e37aecea940109
irmaktekin <irmaktekin97@gmail.com>
2024-06-19 19:05:26 +0300

    CRUD operations for otel are performed.

--- src/dao/HotelDao.java
+++ src/dao/HotelDao.java
@@ -1,7 +1,9 @@
 package dao;
 
 import core.DbConnector;
+import entity.HostelType;
 import entity.Hotel;
+import entity.User;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -39,5 +41,97 @@ public class HotelDao {
         return hotel;
 
 
+    }
+    public boolean updateHotel(Hotel hotel) {
+        String query = "Update public.hotel Set " +
+                "hotel_name = ? , " +
+                "address = ? , " +
+                "mail = ? , " +
+                "phone_number = ? " +
+                "star = ? " +
+                "hostel_type = ? " +
+                "Where hotel_id = ?";
+        try{
+            PreparedStatement pr = connection.prepareStatement(query);
+            pr.setString(1,hotel.getHotel_name());
+            pr.setString(2, hotel.getAddress());
+            pr.setString(3,hotel.getMail());
+            pr.setInt(4,hotel.getPhone_number());
+            pr.setInt(5,hotel.getStar());
+           // pr.setString(6,hotel.getHostel_type().getHostelTypeInfo());
+            pr.setInt(7,hotel.getHotel_id());
+            return pr.executeUpdate() != -1;
+        }
+        catch (SQLException e){
+            e.printStackTrace();
+        }
+        return true;
+    }
+    public boolean createHotel(Hotel hotel) {
+        String query = "INSERT INTO public.hotel "+
+                "(" +
+                "hotel_name," +
+                "address," +
+                "mail," +
+                "phone_number,"+
+                "star," +
+                ")"+
+                " Values (?,?,?,?,?,?)";
+        try{
+            PreparedStatement pr = connection.prepareStatement(query);
+            pr.setString(1,hotel.getHotel_name());
+            pr.setString(2, hotel.getAddress());
+            pr.setString(3,hotel.getMail());
+            pr.setInt(4,hotel.getPhone_number());
+            pr.setInt(5,hotel.getStar());
+            return pr.executeUpdate() != -1;
+        }
+        catch (SQLException e){
+            e.printStackTrace();
+        }
+        return true;
+    }
+    public boolean deleteById(int id){
+        String query = "Delete From public.hotel Where hotel_id = ?";
+        try{
+            PreparedStatement pr = connection.prepareStatement(query);
+            pr.setInt(1,id);
+            return pr.executeUpdate() !=-1;
+        }
+        catch (SQLException e){
+            e.printStackTrace();
+        }
+        return true;
+    }
+    public Hotel getById(int id){
+        Hotel hotelObj = null;
+        String query = "Select * From public.hotel where hotel_id = ?";
+        try{
+            PreparedStatement pr = connection.prepareStatement(query);
+            pr.setInt(1,id);
+            ResultSet rs = pr.executeQuery();
+            if(rs.next()) hotelObj = this.mapResultSetToHotel(rs);
+        }
+        catch (SQLException e){
+            e.printStackTrace();
+        }
+        return hotelObj;
+    }
+
+    public Hotel mapResultSetToHotel(ResultSet rs){
+        Hotel hotel = new Hotel();
+        try{
+            hotel.setHotel_id(rs.getInt("hotel_id"));
+            hotel.setHotel_name(rs.getString("hotel_name"));
+            hotel.setMail(rs.getString("mail"));
+            hotel.setAddress(rs.getString("address"));
+            hotel.setPhone_number(rs.getInt("phone_number"));
+            hotel.setStar(rs.getInt("star"));
+           // hotel.setHostel_type(HostelType.valueOf(rs.getString("hostel_type")));
+        }
+        catch (SQLException e){
+            throw new RuntimeException(e);
+        }
+        return hotel;
     }
 }
